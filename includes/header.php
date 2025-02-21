9d44ba87fe472a553edeb4f4cd00cc230b7771fc
Jose6348 <jfalasco88@gmail.com>
2025-02-21 00:09:52 -0300

    cabeçalho do projeto

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfólio</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="js/scripts.js" defer></script>
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Início</a></li>
+            <li><a href="about.php">Sobre Mim</a></li>
+            <li><a href="projects.php">Projetos</a></li>
+            <li><a href="contact.php">Contato</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
